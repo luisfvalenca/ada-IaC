a686439713daab9361a03a33b4b68390e3eb5540
luisfvalenca <luisfvalenca@gmail.com>
2024-05-13 16:32:44 -0300

    Fix em provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # Provedor AWS
 provider "aws" {
-	access_key = var.access_key
-	secret_key = var.secret_key
+	access_key = var.aws_access_key
+	secret_key = var.aws_secret_key
 	region = var.aws_region
 #	s3_use_path_style = true
 #	endpoints {
