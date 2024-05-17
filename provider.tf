d1ce8c72663bdf4fc8b671fbf22361b642262f40
luisfvalenca <luisfvalenca@gmail.com>
2024-05-17 15:43:43 -0300

    Remove coments antigos

--- provider.tf
+++ provider.tf
@@ -3,9 +3,4 @@ provider "aws" {
 	access_key = var.aws_access_key
 	secret_key = var.aws_secret_key
 	region = var.aws_region
-#	s3_use_path_style = true
-#	endpoints {
-#		ec2 = "http://localhost:4566"
-#		s3 = "http://s3.localhost.localstack.cloud:4566"
-#	}
 }
