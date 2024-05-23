40d4e1feffbd967faa973a76e72651f19acee8b0
luisfvalenca <luisfvalenca@gmail.com>
2024-05-23 12:43:22 -0300

    Testando solução trocando nome das variáveis

--- ami_ec2.tf
+++ ami_ec2.tf
@@ -6,7 +6,10 @@ resource "aws_instance" "app" {
 	subnet_id = aws_subnet.ec2_subnet.id
 	associate_public_ip_address = true
 	vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-	user_data = templatefile("${path.module}/extra_files/config_user.sh", { ansible_user = "${var.ansible_user}", ansible_pubkey = "${var.ansible_pubkey}"})
+	user_data = templatefile("${path.module}/extra_files/config_user.sh", {
+		tf_ansible_user = "${var.ansible_user}",
+		tf_ansible_pubkey = "${var.ansible_pubkey}"
+	})
 	tags = {
 		Name = "app-${var.project_name}-${terraform.workspace}"
 		Environment = terraform.workspace
--- extra_files/config_user.sh
+++ extra_files/config_user.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Captura variaveis passadas pelo Terraform
+USER=${tf_ansible_user}
+PUBKEY=${tf_ansible_pubkey}
+
 # Garante pacotes necessarios ao ansible instalados na instancia
 sudo apt update
 sudo apt install python3 openssh-server -y
 
 # Cria e configura o usuario do ansible
-echo "Criando user: $ansible_user ..."
+echo "Criando user: $USER ..."
 sudo useradd \
     --create-home \
     --comment "Usuario para o Ansible" \
     --shell /bin/bash \
-    $ansible_user
+    $USER
 # Desabilita senha
-sudo passwd -q -d $ansible_user
-sudo passwd -q -l $ansible_user
+sudo passwd -q -d $USER
+sudo passwd -q -l $USER
 # Escreve chave publica
-sudo mkdir -p /home/$ansible_user/.ssh
-sudo echo "$ansible_pubkey" > /home/$ansible_user/.ssh/authorized_keys
+sudo mkdir -p /home/$USER/.ssh
+sudo echo "$PUBKEY" > /home/$USER/.ssh/authorized_keys
 # Da permissao de sudo
-sudo echo "$ansible_user ALL=NOPASSWD: ALL" > /etc/sudoers.d/$ansible_user
+sudo echo "$USER ALL=NOPASSWD: ALL" > /etc/sudoers.d/$USER
