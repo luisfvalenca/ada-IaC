fa1e07f9983b76c18599a7a3257b52d36428d06f
luisfvalenca <luisfvalenca@gmail.com>
2024-05-23 15:04:23 -0300

    Adicionando bloco de configuração de ssh para permitir acesso do usuário Ansible

--- extra_files/config_user.sh
+++ extra_files/config_user.sh
@@ -21,5 +21,13 @@ sudo passwd -q -l $USER
 # Escreve chave publica
 sudo mkdir -p /home/$USER/.ssh
 sudo echo "$PUBKEY" > /home/$USER/.ssh/authorized_keys
+# Adiciona ao AllowGroups do ssh
+sudo sed -i '/RSAAuth/d' /etc/ssh/sshd_config
+sudo sed -i '/PubkeyAuth/d' /etc/ssh/sshd_config
+sudo sed -i '/AllowGroups/d' /etc/ssh/sshd_config
+sudo echo 'RSAAuthentication yes' >> /etc/ssh/sshd_config
+sudo echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
+sudo echo "AllowGroups $USER" >> /etc/ssh/sshd_config
+sudo systemctl restart sshd nscd
 # Da permissao de sudo
 sudo echo "$USER ALL=NOPASSWD: ALL" > /etc/sudoers.d/$USER
